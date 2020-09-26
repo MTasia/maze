90b5f205b387a60105c561a72042fe82dc1784b6
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2020-09-26 11:46:05 +0300

    Add files via upload

--- task/find_count_steps.py
+++ task/find_count_steps.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def on_border(n_rows, n_cols, point):
     """
     Parameters:
@@ -6,6 +9,9 @@ def on_border(n_rows, n_cols, point):
     :point (tuple): point with x and y for check
     :return: bool value that tells whether a point is on the border
     """
+    i = int(point[0])
+    j = int(point[1])
+    return 0 == i or i == n_rows - 1 or 0 == j or j == n_cols - 1
 
 
 def possible_neighbours(point):
@@ -14,12 +20,20 @@ def possible_neighbours(point):
     :point (tuple): point with x and y for possible neighbours
     :return: list of nearby neighbors for point
     """
+    neighbours = []
+    x = point[1]
+    y = point[0]
+    neighbours.append((x, y + 1))
+    neighbours.append((x + 1, y))
+    neighbours.append((x, y - 1))
+    neighbours.append((x - 1, y))
+    return neighbours
 
 
 def count_steps(start, end, prev_step_map):
     """
    Parameters:
-   :start (tuple): start point with x and y 
+   :start (tuple): start point with x and y
    :end (tuple): end point
    :prev_step_map(dict): the dictionary with previous step map
    :return: count steps
@@ -33,3 +47,39 @@ def count_steps_to_exit(maze, start):
    :start (tuple): start point with x and y
    :return: count steps
    """
+    j = start[0]
+    i = start[1]
+    we_were = [[False for j in range(len(maze[0]))] for i in range(len(maze))]
+    dist = [[0 for j in range(len(maze[0]))] for i in range(len(maze))]
+    queue = deque()
+    queue.append((i, j))
+    while queue:
+        we_in = queue.popleft()
+        we_were[we_in[0]][we_in[1]] = True
+        neighbours = need_neighbours(we_in[0], we_in[1], maze, we_were)
+        if len(neighbours) == 0 and not queue:
+            return 0
+        for neighbour in neighbours:
+            dist[neighbour[0]][neighbour[1]] = dist[we_in[0]][we_in[1]] + 1
+            queue.append(neighbour)
+            point = (neighbour[0], neighbour[1])
+            if on_border(len(maze), len(maze[0]), point):
+                step = dist[neighbour[0]][neighbour[1]]
+                if step != 0:
+                    step += 1
+                return step
+
+
+def need_neighbours(i, j, maze, we_were):
+    neighbours = []
+    point = (j, i)
+    if not on_border(len(maze), len(maze[0]), point):
+        if maze[i + 1][j] == 1 and not we_were[i + 1][j]:
+            neighbours.append((i + 1, j))
+        if maze[i][j + 1] == 1 and not we_were[i][j + 1]:
+            neighbours.append((i, j + 1))
+        if maze[i - 1][j] == 1 and not we_were[i - 1][j]:
+            neighbours.append((i - 1, j))
+        if maze[i][j - 1] == 1 and not we_were[i][j - 1]:
+            neighbours.append((i, j - 1))
+    return neighbours
--- task/test_public.py
+++ task/test_public.py
@@ -27,7 +27,7 @@ TEST_CASES_FOR_COUNT_STEPS = [
                                          [0, 1, 1, 1, 0],
                                          [0, 1, 0, 1, 0],
                                          [0, 1, 1, 1, 0],
-                                         [0, 0, 0, 1, 0]], (1, 1)), expected=5),
+                                         [0, 0, 0, 1, 0]], (1, 1)), expected=6),
     Case(name='cycle_without_exit', input=([[0, 0, 0, 0, 0],
                                             [0, 1, 1, 1, 0],
                                             [0, 1, 0, 1, 0],
@@ -38,10 +38,10 @@ TEST_CASES_FOR_COUNT_STEPS = [
 TEST_CASES_FOR_BORDER = [
     Case(name='in', input=(5, 5, (6, 6)), expected=False),
     Case(name='out', input=(5, 5, (3, 3)), expected=False),
-    Case(name='left', input=(5, 4, (4, 4)), expected=True),
-    Case(name='right', input=(4, 5, (4, 4)), expected=True),
-    Case(name='up', input=(5, 5, (0, 5)), expected=True),
-    Case(name='down', input=(5, 5, (5, 0)), expected=True)
+    Case(name='left', input=(5, 4, (4, 3)), expected=True),
+    Case(name='right', input=(4, 5, (2, 4)), expected=True),
+    Case(name='up', input=(5, 5, (0, 3)), expected=True),
+    Case(name='down', input=(5, 5, (2, 0)), expected=True)
 ]
 
 TEST_CASES_FOR_NEIGHBOURS = [
@@ -50,18 +50,18 @@ TEST_CASES_FOR_NEIGHBOURS = [
 
 
 @pytest.mark.parametrize('exit', TEST_CASES_FOR_COUNT_STEPS, ids=str)
-def test_count_steps_to_exit(test_case: Case) -> None:
-    answer = count_steps_to_exit(*test_case.input)
-    assert answer == test_case.expected
+def test_count_steps_to_exit(exit: Case) -> None:
+    answer = count_steps_to_exit(*exit.input)
+    assert answer == exit.expected
 
 
 @pytest.mark.parametrize('border', TEST_CASES_FOR_BORDER, ids=str)
-def test_on_border(test_case: Case) -> None:
-    answer = on_border(*test_case.input)
-    assert answer == test_case.expected
+def test_on_border(border: Case) -> None:
+    answer = on_border(*border.input)
+    assert answer == border.expected
 
 
-@pytest.mark.parametrize('neigbours', TEST_CASES_FOR_NEIGHBOURS, ids=str)
-def test_possible_neighbours(test_case: Case) -> None:
-    answer = possible_neighbours(test_case.input)
-    assert answer == test_case.expected
+@pytest.mark.parametrize('neighbours', TEST_CASES_FOR_NEIGHBOURS, ids=str)
+def test_possible_neighbours(neighbours: Case) -> None:
+    answer = possible_neighbours(neighbours.input)
+    assert answer == neighbours.expected
